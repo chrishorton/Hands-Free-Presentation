bbf45dc2a593249733315502724e0b374e2f6d5a
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2017-05-20 20:58:57 -0700

    Edits

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ def left_key_press():
 def right_key_press():
     pyautogui.press('right')
 
-
-def main():
-    next_detector = snowboydecoder.HotwordDetector("models/next_slide.pmdl", sensitivity=0.5, audio_gain=1)
-    previous_detector = snowboydecoder.HotwordDetector("models/previous_slide.pmdl", sensitivity=0.5, audio_gain=1)
-    next_detector.start(right_key_press)
-    previous_detector.start(left_key_press)
-
-if __name__=='__main__':
-    main()
+next_detector = snowboydecoder.HotwordDetector("models/next_slide.pmdl", sensitivity=0.5, audio_gain=1)
+previous_detector = snowboydecoder.HotwordDetector("models/previous_slide.pmdl", sensitivity=0.5, audio_gain=1)
+next_detector.start(right_key_press)
+previous_detector.start(left_key_press)

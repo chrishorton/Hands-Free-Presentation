2adf91dddbd2caf850d0cc06a98c4cc79c2355e0
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2017-05-21 01:57:51 -0700

    Add comments and move snowboy files to separate package

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pyautogui
-import snowboydecoder
+from snowboy import snowboydecoder
 import threading
 import time
 
+
 class DetectorThread (threading.Thread):
+    """
+    Thread class for the separate detectors to detect simultaneously
+    """
     def __init__(self, detector, callback):
         threading.Thread.__init__(self)
         self.detector = detector
@@ -21,13 +25,16 @@ def left_key_press():
 def right_key_press():
     pyautogui.press('right')
 
-next_detector = snowboydecoder.HotwordDetector("models/next_slide.pmdl", sensitivity=0.5, audio_gain=1)
-previous_detector = snowboydecoder.HotwordDetector("models/previous_slide.pmdl", sensitivity=0.5, audio_gain=1)
-next_thread = DetectorThread(next_detector,right_key_press)
-previous_thread = DetectorThread(previous_detector, left_key_press)
+next_slide_detector = snowboydecoder.HotwordDetector("models/next_slide.pmdl", sensitivity=0.5, audio_gain=1)
+previous_slide_detector = snowboydecoder.HotwordDetector("models/previous_slide.pmdl", sensitivity=0.5, audio_gain=1)
 
-next_thread.start()
-previous_thread.start()
+next_slide_thread = DetectorThread(next_slide_detector, right_key_press)
+previous_slide_thread = DetectorThread(previous_slide_detector, left_key_press)
 
+next_slide_thread.start()
+previous_slide_thread.start()
+
+# Hack to keep main thread running, as since the child threads are daemon threads, main thread can terminate them
 while True:
+    # Hack to reduce CPU usage significantly
     time.sleep(100)

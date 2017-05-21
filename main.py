fe39f40b58890d94e415f3cf21c2bd7625adda97
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2017-05-20 21:45:47 -0700

    Added threading for 2 detectors

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pyautogui
 import snowboydecoder
+import threading
+
+
+class DetectorThread (threading.Thread):
+    def __init__(self, detector):
+        super.__init__(self)
+        self.detector = detector
+
+    def run(self):
+        self.detector.start()
 
 
 def left_key_press():
@@ -11,5 +21,8 @@ def right_key_press():
 
 next_detector = snowboydecoder.HotwordDetector("models/next_slide.pmdl", sensitivity=0.5, audio_gain=1)
 previous_detector = snowboydecoder.HotwordDetector("models/previous_slide.pmdl", sensitivity=0.5, audio_gain=1)
-next_detector.start(right_key_press)
-previous_detector.start(left_key_press)
+next_thread = DetectorThread(next_detector)
+previous_thread = DetectorThread(previous_detector)
+
+next_thread.start()
+previous_thread.start()

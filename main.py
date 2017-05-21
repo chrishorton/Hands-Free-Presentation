d4deaf4a722810a6104ab0d7f84427b1eca111fe
Chris Horton <chris.horton@sbcglobal.net>
2017-05-20 22:01:09 -0700

    final project

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import threading
 
 
 class DetectorThread (threading.Thread):
-    def __init__(self, detector):
+    def __init__(self, detector, callback):
         threading.Thread.__init__(self)
         self.detector = detector
+        self.callback = callback
+        self.daemon = True
 
     def run(self):
-        self.detector.start()
+        self.detector.start(self.callback)
 
 
 def left_key_press():
@@ -21,8 +23,11 @@ def right_key_press():
 
 next_detector = snowboydecoder.HotwordDetector("models/next_slide.pmdl", sensitivity=0.5, audio_gain=1)
 previous_detector = snowboydecoder.HotwordDetector("models/previous_slide.pmdl", sensitivity=0.5, audio_gain=1)
-next_thread = DetectorThread(next_detector)
-previous_thread = DetectorThread(previous_detector)
+next_thread = DetectorThread(next_detector,right_key_press)
+previous_thread = DetectorThread(previous_detector, left_key_press)
 
 next_thread.start()
 previous_thread.start()
+
+while True:
+    pass

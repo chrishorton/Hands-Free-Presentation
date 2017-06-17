16ef3008baef6e6a3938bb6940902616baf00880
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2017-06-17 00:30:35 -0700

    Wrap detectors in a class to contain data and provide methods for starting and stopping

--- main.py
+++ main.py
@@ -2,40 +2,55 @@ import pyautogui
 from snowboy import snowboydecoder
 import signal
 
-interrupted = False
-
 
 def left_key_press():
     pyautogui.press('left')
-    print("Heard previous slide - executing left key press")
+    print('Heard "previous slide" - executing left key press')
 
 
 def right_key_press():
     pyautogui.press('right')
-    print("Heard next slide - executing right key press")
+    print('Heard "next slide" - executing right key press')
+
+
+class Detectors(object):
+    """
+    Wrapper class around detectors to incorporate methods and data
+    """
+
+    def __init__(self, detect_models=None, sensitivity=None):
+        if detect_models is not None:
+            self.models = detect_models
+        else:
+            self.models = ["models/next_slide.pmdl", "models/previous_slide.pmdl"]
+
+        if sensitivity is not None:
+            self.sensitivity_args = sensitivity
+        else:
+            self.sensitivity_args = [0.5] * len(self.models)
+
+        self.interrupted = False
+        self.callbacks = [right_key_press, left_key_press]
+        self.detectors = snowboydecoder.HotwordDetector(self.models, sensitivity=self.sensitivity_args)
+
+    def start_detecting(self):
+        print('Now listening for hot works "Next Slide" and "Previous Slide"')
+        self.interrupted = False
+        self.detectors.start(detected_callback=self.callbacks,
+                             interrupt_check=lambda: self.interrupted)
+        self.detectors.terminate()
+
+    def stop_detecting(self):
+        self.interrupted = True
 
 
 def signal_handler(signal_received, frame):
-    global interrupted
-    interrupted = True
-
-
-def interrupt_callback():
-    global interrupted
-    return interrupted
+    detectors.stop_detecting()
 
 # Capture and handle CTRL-C
 signal.signal(signal.SIGINT, signal_handler)
 
-models = ["models/next_slide.pmdl", "models/previous_slide.pmdl"]
-sensitivity_args = [0.5] * len(models)
-callbacks = [right_key_press, left_key_press]
-detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity_args)
+detectors = Detectors()
+detectors.start_detecting()
 
-print('Now listening for hot words "Next Slide" and "Previous Slide"')
-
-detector.start(detected_callback=callbacks,
-               interrupt_check=interrupt_callback)
-
-detector.terminate()
 print("Quitting program...")
